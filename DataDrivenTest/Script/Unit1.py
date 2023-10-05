0970db4279f7632b973b70d17184e7a8718900b6
fpaulson001 <fpaulson@guidehouse.com>
2023-10-05 05:03:01 -0500

    Changes are updated to commit

---
+++ DataDrivenTest/Script/Unit1.py
@@ -0,0 +1,4 @@
+﻿def Test():
+  windows = Sys.FindAllChildren("VisibleOnScreen", "true", 2)
+  if (l := len(windows)) > 0:
+    Log.Message(aqString.Format("There are %i visible windows on the screen", l))
